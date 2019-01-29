d6b6a0be7766dfdd20a4ba2b142194ecd536f907
Vince Bloise <vbloise3@verizon.net>
2019-01-28 21:18:44 -0500

    added chalice api functionality\!

--- mbloiseResume3Ui/buildDeploy.sh
+++ mbloiseResume3Ui/buildDeploy.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 #/bin/bash
+#build the chalice-javascript-jdk
+cd ~/mbloiseResume3/mbloiseResume3Ui/src/chalice-javascript-sdk/
+rm -r *
+cd ~/mbloiseResume3/mbloiseResume3Ui/src/
+rmdir chalice-javascript-sdk
+cd ~/mbloiseResume3/
+chalice generate-sdk ./mbloiseResume3Ui/src
+#build the site
+cd ~/mbloiseResume3/mbloiseResume3Ui/
 #build the site
 ng build --prod --aot
 #upload files
-aws s3 cp ./dist/mbloiseResume3Ui s3://mbloiseResume3Ui --recursive --acl public-read
+aws s3 cp ./dist/mbloiseResume3Ui s3://mbloise-resume --recursive --acl public-read
