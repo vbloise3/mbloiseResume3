974f48b097d0678c3b34c84049036016220e877a
Vince Bloise <vbloise3@gmail.com>
2021-12-12 13:51:25 -0500

    Upgraded to Angular 13

--- mbloiseResume3Ui/buildDeploy.sh
+++ mbloiseResume3Ui/buildDeploy.sh
@@ -10,7 +10,7 @@
 #build the site
 cd ~/mbloiseResume3/mbloiseResume3Ui/
 #build the site
-ng build --prod --aot
+ng build --configuration production --aot
 #upload files
 aws s3 cp ./dist/mbloiseResume3Ui s3://marinbloise.com --recursive --acl public-read
 aws s3 cp ./dist/mbloiseResume3Ui s3://www.marinbloise.com --recursive --acl public-read
